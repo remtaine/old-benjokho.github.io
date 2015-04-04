2b725f3e02fb41c398296397356a9c06339b4335
Benjo Kho <nb2kho1@gmail.com>
2015-04-04 14:18:45 +0800

    bouncy ball

--- js/game.js
+++ js/game.js
@@ -1,4 +1,13 @@
-$(document).ready(function() {
+var canvas;
+var stage;
 
+function init() {
+	stage = new createjs.Stage("demoCanvas");
 
-});
+	var circle = new createjs.Shape();
+	circle.graphics.beginFill("red").drawCircle(0, 0, 50);
+	circle.x = 100;
+	circle.y = 100;
+	stage.addChild(circle);
+	stage.update();
+}
